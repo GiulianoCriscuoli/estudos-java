e1d55988f2ea0e04bc54ea25036011d378f37a7e
GiulianoCriscuoli <giulianodias3@hotmail.com>
2024-05-31 11:22:59 -0300

    criação do projeto conta bancária

---
+++ conta-bancaria/src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        double saldo = 237.0;
+        String agencia = "067-8";
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Informe o seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Informe o numero da agência: ");
+        int numero = scanner.nextInt();
+
+
+        System.out.println("Usuário: " +  numero);
+
+        System.out.println("Olá " + nome + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+    }
+}
